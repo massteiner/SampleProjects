0847aa8b131de28746623343e7095e4b09776b56
Marcel Steiner <m.steiner84@bluewin.ch>
2015-08-21 17:56:15 +0200

    add colore

--- UniversalTestApp/UniversalTestApp/MainPage.xaml.cs
+++ UniversalTestApp/UniversalTestApp/MainPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -37,6 +39,22 @@ namespace UniversalTestApp
             {
                 MainSplitView.DisplayMode = SplitViewDisplayMode.CompactOverlay;
             }
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new MessageDialog("Hello World");
+            dialog.ShowAsync();
+        }
+
+        private void ButtonChangeColore_OnClick(object sender, RoutedEventArgs e)
+        {
+            var random = new Random();
+            var a = BitConverter.GetBytes(random.Next(254))[0];
+            var r = BitConverter.GetBytes(random.Next(254))[0];
+            var g = BitConverter.GetBytes(random.Next(254))[0];
+            var b = BitConverter.GetBytes(random.Next(254))[0];
+            scvMain.Background = new SolidColorBrush(Color.FromArgb(a, r, g, b));
         }
     }
 }
